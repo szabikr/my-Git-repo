fb738490316b984d8796426b5e789d2979010224
nafisoaden97 <103136799+Udecunup5@users.noreply.github.com>
2022-05-10 17:22:35 +0100

    week 2 hackathon
    we created a trivia pop quiz using fetch method and asyn function. we had some difficulties using the asyn Functions

---
+++ day-4Hackathon/w2d5_fetch-hackathon-room-39-arian-nafiso/main.js
@@ -0,0 +1,223 @@
+// API data
+    // Create an async function to retrieve the data in JSON using fetch;
+    // Convert that response data into a javascript object;
+    // console.log the data
+
+    let i =0;
+    let questionsAnsweredCount = 0;
+    let correctAnswersCount = 0;
+    let incorrectAnswersCount = 0;
+    let questionsAnswered = document.querySelector("#questions-answered");
+    let correctAnswers = document.querySelector("#correct");
+    let incorrectAnswers = document.querySelector("#incorrect");
+    let h1 = document.querySelector("h1");
+    let a1 = document.querySelector("#a1");
+    let a2 = document.querySelector("#a2");
+    let a3 = document.querySelector("#a3");
+    let a4 = document.querySelector("#a4");
+
+    let b1= document.querySelector('#b1')
+    let b2= document.querySelector('#b2')
+    let b3= document.querySelector('#b3')
+    let b4= document.querySelector('#b4')
+    let playAgain= document.querySelector('#playAgain')
+
+    let username = prompt("What is your name?")
+
+    let greeting = document.querySelector("#greeting");
+greeting.innerText = `Hello, ${username}!`
+
+    let question = document.querySelector("#question")
+    getTrivia()
+
+    
+async function getTrivia(){
+    let response = await fetch("https://opentdb.com/api.php?amount=10");
+    let data = await response.json();
+    console.log(data)
+    let question = document.querySelector("#question")
+    question.innerText = Convert(data.results[i].question);
+    let arr1 = [data.results[i].correct_answer,data.results[i].incorrect_answers[0],data.results[i].incorrect_answers[1],data.results[i].incorrect_answers[2]];
+    console.log(arr1);
+    if(arr1[0] == "True" || arr1[0] == "False"){
+        arr1.splice(2,2);
+        b3.style.visibility = "hidden"
+        b4.style.visibility = "hidden"
+        a3.style.visibility = "hidden"
+        a4.style.visibility = "hidden"
+    }
+    let arr2 = randomiseArr(arr1);
+    console.log(arr2)
+    a1.innerText = arr2[0];
+    a2.innerText = arr2[1];
+    a3.innerText = arr2[2];
+    a4.innerText = arr2[3];
+    b1.addEventListener('click',(e) => {
+        b1.style.visibility = 'hidden'
+        b2.style.visibility = 'hidden'
+        b3.style.visibility = 'hidden'
+        b4.style.visibility = 'hidden'
+        playAgain.style.visibility = 'visible'
+        a2.innerText = ""
+        a3.innerText = ""
+        a4.innerText = ""
+
+        if (a1.innerText === data.results[i].correct_answer){
+            a1.style.color = "green"
+            correctAnswersCount++;
+            questionsAnsweredCount++;
+            correctAnswers.innerText = `Correct Answers: ${correctAnswersCount}`
+            questionsAnswered.innerText = `Questions Answered: ${questionsAnsweredCount}`
+            
+        }
+            
+
+        else{
+                a1.style.color = "red"
+                incorrectAnswersCount++;
+                questionsAnsweredCount++;
+                incorrectAnswers.innerText = `Incorrect Answers: ${incorrectAnswersCount}`
+                questionsAnswered.innerText = `Questions Answered: ${questionsAnsweredCount}`
+            }
+            
+    })
+    b2.addEventListener('click',(e) => { 
+        b1.style.visibility = 'hidden'
+        b2.style.visibility = 'hidden'
+        b3.style.visibility = 'hidden'
+        b4.style.visibility = 'hidden'
+        playAgain.style.visibility = 'visible'
+        a1.innerText = ""
+        a3.innerText = ""
+        a4.innerText = ""
+
+        if (a2.innerText === data.results[i].correct_answer){
+            
+        a2.style.color = "green"
+        correctAnswersCount++;
+        correctAnswers.innerText = `Correct Answers: ${correctAnswersCount}`
+        questionsAnsweredCount++
+        questionsAnswered.innerText = `Questions Answered: ${questionsAnsweredCount}`}
+            else{
+                
+                a2.style.color = "red"
+                incorrectAnswersCount++;
+                questionsAnsweredCount++;
+                incorrectAnswers.innerText = `Incorrect Answers: ${incorrectAnswersCount}`
+                questionsAnswered.innerText = `Questions Answered: ${questionsAnsweredCount}`
+            }
+            
+    })
+    b3.addEventListener('click',(e) => {
+        b1.style.visibility = 'hidden'
+        b2.style.visibility = 'hidden'
+        b3.style.visibility = 'hidden'
+        b4.style.visibility = 'hidden'
+        playAgain.style.visibility = 'visible'
+        a1.innerText = ""
+        a2.innerText = ""
+        a4.innerText = ""
+        if (a3.innerText === data.results[i].correct_answer){
+            
+            a3.style.color = "green"
+            correctAnswersCount++;
+            correctAnswers.innerText = `Correct Answers: ${correctAnswersCount}`
+            questionsAnsweredCount++
+            questionsAnswered.innerText = `Questions Answered: ${questionsAnsweredCount}`}
+            else{
+                a3.style.color = "red"
+                incorrectAnswersCount++;
+                questionsAnsweredCount++;
+                incorrectAnswers.innerText = `Incorrect Answers: ${incorrectAnswersCount}`
+                questionsAnswered.innerText = `Questions Answered: ${questionsAnsweredCount}`
+            }
+            
+    })
+    b4.addEventListener('click',(e) => {
+        b1.style.visibility = 'hidden'
+        b2.style.visibility = 'hidden'
+        b3.style.visibility = 'hidden'
+        b4.style.visibility = 'hidden'
+        playAgain.style.visibility = 'visible'
+        a1.innerText = ""
+        a2.innerText = ""
+        a3.innerText = ""
+        if (a4.innerText === data.results[i].correct_answer){
+            a4.style.color = "green"
+            correctAnswersCount++;
+            correctAnswers.innerText = `Correct Answers: ${correctAnswersCount}`
+            questionsAnsweredCount++;
+            questionsAnswered.innerText = `Questions Answered: ${questionsAnsweredCount}`}
+            else{
+                a4.style.color ="red"
+                incorrectAnswersCount++;
+                questionsAnsweredCount++;
+                incorrectAnswers.innerText = `Incorrect Answers: ${incorrectAnswersCount}`
+                questionsAnswered.innerText = `Questions Answered: ${questionsAnsweredCount}`
+            }
+            
+    })
+    playAgain.addEventListener("click",(e) => {
+        playAgain.style.visibility = "hidden";
+        if(a1.innerText === "True" || a1.innerText === "False"){
+            a3.style.visibility = "visible";
+            a4.style.visibility = "visible";
+        }
+        
+        
+        b1.style.visibility = 'visible'
+        b2.style.visibility = 'visible'
+        b3.style.visibility = 'visible'
+        b4.style.visibility = 'visible'
+        a1.style.color = "black"
+        a2.style.color = "black"
+        a3.style.color = "black"
+        a4.style.color = "black"
+        
+        getTrivia();
+    })
+
+
+
+    
+} 
+
+
+
+
+
+// b1.addEventListener('click',(e) => {
+//     if (a1.innerText === data.results[0].correct_answer){
+//         alert("you win!")}
+        
+  
+    
+// })
+//Plan
+
+//Take in a username (restrict username to 10chars and maybe capitalise the first letter)
+//Question at the top insert to H1;
+//created list 
+//insert data.results.incorrect/correct answers into list
+//create buttons for each answer
+//button=Options for answers, 1 correct and 3 incorrect;
+//Buttons next to the answer and they can select;
+//Right answer: Give a score;
+//Wrong answer: Do nothing, send a message;
+
+
+
+function randomiseArr(array){
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex !==0){
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--; 
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex],array[currentIndex]];
+    }
+    return array;
+}
+
+function Convert(string){
+   let newString =  string.replace(/&quot;/g, `"`).replace(/&#039;/g,`'`)
+    return newString;
+}
